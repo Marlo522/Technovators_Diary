db625cf1d4ab170881c4d9c05cb8a394fd6ca3f3
Marlo522 <202311932@fit.edu.ph>
2025-07-17 12:25:55 +0800

    feat: Implement view binding in Login activity and add navigation to SignUp screen

--- app/src/main/java/com/example/technovatorsdiary/Login.kt
+++ app/src/main/java/com/example/technovatorsdiary/Login.kt
@@ -1,20 +1,28 @@
 package com.example.technovatorsdiary
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.technovatorsdiary.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.tvSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
         }
     }
 }
