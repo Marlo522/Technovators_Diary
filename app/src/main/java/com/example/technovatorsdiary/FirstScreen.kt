3b905c142efe3815556690f92b87682147eaf621
Marlo522 <202311932@fit.edu.ph>
2025-07-17 11:23:51 +0800

    feat: Implement navigation from first screen to login screen using ViewBinding

--- app/src/main/java/com/example/technovatorsdiary/FirstScreen.kt
+++ app/src/main/java/com/example/technovatorsdiary/FirstScreen.kt
@@ -1,20 +1,29 @@
 package com.example.technovatorsdiary
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.technovatorsdiary.databinding.ActivityFirstScreenBinding
 
 class FirstScreen : AppCompatActivity() {
+    private  lateinit var binding: ActivityFirstScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_first_screen)
+        binding = ActivityFirstScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnContinue.setOnClickListener {
+
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
         }
     }
 }
