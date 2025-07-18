a025f5a28642c894694b5f1e094e6e70a08576a6
carlmarinyo <marinojerome2@gmail.com>
2025-07-18 16:32:03 +0800

    feat: add profile icon and activity
    This commit introduces a profile icon to the dashboard and adds a new `Profile` activity.
    
    Key changes:
    - Added a profile icon (`profile.xml`) to the dashboard layout (`activity_dashboard.xml`).
    - Created a new `Profile` activity and declared it in `AndroidManifest.xml`.
    - Updated the logout button text in `activity_profile.xml`.
    - Removed unused code related to displaying user information in `Dashboard.kt`.
    - Renamed `lumenText` to `tvTechnovators` in `activity_dashboard.xml`.

--- app/src/main/java/com/example/technovatorsdiary/Dashboard.kt
+++ app/src/main/java/com/example/technovatorsdiary/Dashboard.kt
@@ -8,24 +8,17 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.technovatorsdiary.databinding.ActivityDashboardBinding
 
 class Dashboard : AppCompatActivity() {
-    private lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityDashboardBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val firstName = intent.getStringExtra("firstName") ?: ""
-        val lastName = intent.getStringExtra("lastName") ?: ""
-        val email = intent.getStringExtra("email") ?: ""
 
-        binding.tvFirstName.text = firstName
-        binding.tvLastName.text = lastName
-        binding.tvEmail.text = email
+
     }
 }
