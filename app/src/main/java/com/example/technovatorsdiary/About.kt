f31706291ae65878a601c4eaebd3548070abeab0
Marlo522 <202311932@fit.edu.ph>
2025-07-20 11:27:31 +0800

    feat: enhance UI for profile, review, login, and sign up screens; add back button to about page

--- app/src/main/java/com/example/technovatorsdiary/About.kt
+++ app/src/main/java/com/example/technovatorsdiary/About.kt
@@ -1,5 +1,6 @@
 package com.example.technovatorsdiary
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +21,11 @@ class About : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnback.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
         }
     }
 }
